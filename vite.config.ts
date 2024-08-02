ee7d2e393e86fe0207693e367e8ca64455db7b4f
rambutan <pride.kraievskyi@gmail.com>
2024-08-02 12:36:19 +0300

    fix fonts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import svgr from "vite-plugin-svgr";
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,6 +13,12 @@ export default defineConfig({
         include: "**/*.svg",
       },
     ),
+    viteStaticCopy({
+      targets: [{
+        src: path.resolve(__dirname, "./src/assets/fonts/*"),
+        dest: path.resolve(__dirname, "./dist/assets/")
+      }]
+    })
   ],
   base: '/t-dar/',
   resolve: {
